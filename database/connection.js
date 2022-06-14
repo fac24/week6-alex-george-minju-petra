13c4be6af3ae82b3a248c944d189d69a76202153
glombort <george.lambert99@gmail.com>
2022-06-14 17:37:33 +0100

    Created Product Pages
    WOOO product PAGES
    
    Co-Authored-By: Minju Kim <48757517+minju25kim@users.noreply.github.com>
    Co-Authored-By: Alex <68148169+lascellesabercrombie@users.noreply.github.com>
    Co-Authored-By: Petra <85565743+alternadiva@users.noreply.github.com>

--- database/connection.js
+++ database/connection.js
@@ -1,23 +1,11 @@
 const pg = require("pg");
 
-<<<<<<< HEAD
 if (!process.env.DATABASE_URL) {
-    throw new Error("DATABASE_URL environment variable is not set");
-}
-
-const db = new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
-});
-
-export default db
-=======
-if (!process.env.local.DATABASE_URL) {
   throw new Error("DATABASE_URL environment variable is not set");
 }
 
 const db = new pg.Pool({
-  connectionString: process.env.local.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 });
 
-module.exports = db;
->>>>>>> main
+export default db;
--- database/model.js
+++ database/model.js
@@ -1,15 +1,36 @@
-import db from "./connection.js"
+import db from "./connection.js";
 
 async function getAllProducts() {
-    const query_text = /* sql */ `
+  const QUERY_TEXT = /* sql */ `
   SELECT products.*, categories.category, brands.brand 
   FROM products
   INNER JOIN categories ON categories.id=products.category_id
   INNER JOIN brands ON brands.id = products.brand_id`;
-    return await db
-        .query(query_text)
-        .then((products) => products)
-        .then(console.log);
+  const query_data = await db.query(QUERY_TEXT);
+  return query_data.rows;
 }
 
-export { getAllProducts }
+async function getProductById(id) {
+  const QUERY_TEXT = /* sql */ `
+  SELECT products.*, categories.category, brands.brand 
+  FROM products
+  INNER JOIN categories ON categories.id=products.category_id AND products.id = $1
+  INNER JOIN brands ON brands.id = products.brand_id`;
+  const query_data = await db.query(QUERY_TEXT, [id]);
+  return query_data.rows[0];
+}
+
+async function getAllProductIds() {
+  const ALL_IDS = `SELECT id FROM products`;
+
+  const ids = await db.query(ALL_IDS).then((result) => result.rows);
+  return ids.map((productId) => {
+    return {
+      params: {
+        id: `${productId.id}`,
+      },
+    };
+  });
+}
+
+export { getAllProducts, getAllProductIds, getProductById };
