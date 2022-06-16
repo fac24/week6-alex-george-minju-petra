5a69c460f35b068eae535ce92d6b7343e62f5df7
Alex Grafen <alex.grafen@gmail.com>
2022-06-16 16:12:04 +0100

    furhter tests around navigation

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -25,14 +25,19 @@ describe('Test our homepage', () => {
     cy.visit("/");
     cy.get('ul > li > a').contains('Acer').click();
     cy.url().should('include', 'products/1');
-    // cy.get('div').contains('soiling and wear');
+    cy.get('div').contains('soiling and wear');
   })
   it('can navigate from homepage to product page', () => {
     cy.visit("/products/1");
     cy.contains('Acer');
     cy.get('a').contains('Back to home').click();
     cy.url().should('eq', '/')
-    // cy.get('div').contains('soiling and wear');
+    cy.get('div').contains('soiling and wear');
+  })
+  it('can navigate from homepage to basket', () => {
+    cy.visit("/");
+    cy.get('#__next > div > header > div > div.flex.flex-row.mt-2 > div > a').contains('Cart').click();
+    cy.url().should('include', 'basket');
   })
 })
 
