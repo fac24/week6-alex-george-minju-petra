af527020aa7ec5d4a09f673fdebdbf68e0b290e7
glombort <george.lambert99@gmail.com>
2022-06-16 16:59:24 +0100

    Fix deploy
    Co-Authored-By: Minju Kim <48757517+minju25kim@users.noreply.github.com>
    Co-Authored-By: Petra <85565743+alternadiva@users.noreply.github.com>
    Co-Authored-By: Alex <68148169+lascellesabercrombie@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Layout from "../components/Layout";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import Filter from "../components/Filter";
 import Link from "next/link";
 import { getAllProducts } from "../database/model";
