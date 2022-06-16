612aa13a2a243aba622b5df133696be238956b89
Alex Grafen <alex.grafen@gmail.com>
2022-06-16 22:23:23 +0100

    introduce pause() and some other things to get tests passing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,3 +1,7 @@
+beforeEach(() => {
+  cy.clearLocalStorage();
+})
+
 describe('Test our homepage', () => {
   it('visiting homepage works', () => {
     cy.visit("/")
@@ -13,12 +17,14 @@ describe('Test our homepage', () => {
   it('can navigate from homepage to product page', () => {
     cy.visit("/");
     cy.contains('a', 'Acer').click();
+    cy.pause();
     cy.url().should('include', 'products/1');
     cy.get('div').contains('soiling and wear');
   })
   it('can navigate from homepage to basket', () => {
     cy.visit("/");
-    cy.contains('a', 'Cart').click();
+    cy.contains('a', 'Basket').click();
+    cy.pause();
     cy.url().should('include', 'basket');
   })
 })
@@ -27,18 +33,19 @@ describe('Product tests', () => {
   it('can navigate from product page to homepage', () => {
     cy.visit("/products/1");
     cy.contains('Acer');
-    cy.contains('a', 'Back to home').click();
-    cy.url().should('eq', '/')
-    cy.get('div').contains('soiling and wear');
+    cy.contains('a', 'Back To Home').click();
+    cy.pause();
+    cy.url().should('not.include', 'products');
+    cy.url().should('not.include', 'basket');
   })
 
   it('can add item to basket from product page', () => {
-    cy.clearLocalStorage();
     cy.visit("/products/1");
     cy.get('button')
       .click()
       .should(() => {
-        expect(localStorage.getItem('basket')).to.eq('{"colours":"Sorta sage","variants":"5g","quantity":"1","pid":"4"},,{"colours":"silver","variants":"large screen","quantity":"1","pid":{"id":1,"name":"Acer Aspire C24","category_id":3,"stock":7,"price":"699.00","description":"Computer with good memory and various screen sizes. Some soiling and wear.","photo_url":"https://static2-ecemea.acer.com/media/catalog/product/cache/4d1e466ee0151a7142143e21e5d254f9/_/a/_acer-aspire_c24-1651_main_kbmouse_dq.bg8ek.003.png","variants":"{\"variants\": [\"large screen\", \"medium screen\", \"small screen\"], \"variantType\": \"Screen Size\"}","colours":"{\"colours\": [\"silver\", \"space grey\", \"burgundy\"]}","brand_id":1,"category":"desktop","brand":"microsoft"},"totalPrice":"699.00"}')
+        expect(localStorage.getItem('basket')).to.include('Acer');
+        expect(localStorage.getItem('basket')).not.to.include('Logitech');
       })
     cy.clearLocalStorage();
   })
