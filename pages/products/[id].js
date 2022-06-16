112838773f246d802fff5cbeef47fae7525b7f5e
glombort <george.lambert99@gmail.com>
2022-06-16 17:04:59 +0100

    Fix deploy 2.0
    Co-Authored-By: Minju Kim <48757517+minju25kim@users.noreply.github.com>
    Co-Authored-By: Petra <85565743+alternadiva@users.noreply.github.com>
    Co-Authored-By: Alex <68148169+lascellesabercrombie@users.noreply.github.com>

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -90,11 +90,17 @@ export default function Products({
               onChange={(event) => setQuantity(event.target.value)}
             />
             {basket ? (
-              <AddBasketButton
+              <button
+                className="bg-purple-200 text-xl rounded-full p-4 hover:bg-purple-400 mt-2 md:text-md"
+                type="submit"
+              >
+                Add to basket
+              </button>
+            ) : /* <AddBasketButton
                 cartTotal={cartTotal}
                 setCartTotal={setCartTotal}
-              />
-            ) : null}
+              /> */ //}
+            null}
           </form>
         </div>
       </section>
